f46b0430bf6734d4a1af74c22908ac1abc31a844
Greendeer30 <markdtgreen@gmail.com>
2026-01-22 12:05:06 -0500

    Hi

--- Checkout/script.js
+++ Checkout/script.js
@@ -1,21 +1,29 @@
 
 async function addData(Id){
 
+    console.log(Id);
+
     const thisDate = (new Date()).toISOString().split("T")[0];
     
     const dataRef = db
         .collection("Data")
-        .doc("Checkout");
+        .doc(thisDate + " - " +Id);
+
+    console.log(document.getElementById("deerCaught").value);
+
+    var deerCaught = parseInt(document.getElementById("deerCaught").value);
+
+    console.log(deerCaught);
 
     await dataRef.set({
-        Checkout: firebase.firestore.FieldValue.arrayUnion(document.getElementById("deerCaught"))
+        Checkout: firebase.firestore.FieldValue.arrayUnion(deerCaught, parseInt(Id))
     }, { merge: true });
 
 }
 
 
 document.getElementById("submit").onclick = function goToCheckout(){
-    addData(document.getElementById("id"));
+    addData(document.getElementById("id").value);
     window.location.replace("https://mdt44.github.io/Hunter/hunter.html");
 }
 
