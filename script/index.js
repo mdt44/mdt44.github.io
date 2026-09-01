39b735c76524959218ef372cee0d6fc25dde2155
Greendeer30 <markdtgreen@gmail.com>
2026-09-01 11:35:59 -0400

    broke it

--- script/index.js
+++ script/index.js
@@ -4,23 +4,50 @@ const LOGIN = document.getElementById("login");
 
 LOGIN.addEventListener("click", () => {
     if(ID.value >= 1 && ID.value <= 99){
-        checkLoginInfo(ID.value, NAME.value);
+        checkInitial(ID.value, NAME.value);
     }
 });
 
-async function checkLoginInfo(id, name){
-
-    const doc = await db
-        .collection("login")
+async function checkInitial(number, initial, date = new Date()){
+    
+    const hunterIDRef = db
+        .collection("reserved")
         .doc("hunters")
-        .get();
+        .collection("hunterID")
+        .doc("h" + number);
+    
+    var snap = await hunterIDRef.get();
+    var hunterID = await snap.data()?.initial;
 
-    const correctName = doc.data()[String(id)];
+    console.log(number);
+    console.log(initial);
+    console.log(hunterID);
+    
+    if(hunterID === initial){
+        hID = number;
+        sessionStorage.setItem("hunterID", hID);
+        
+        const thisDate = typeof date === "string" ? date : getLocalDateKey(date);
 
-    if(correctName.toLowerCase() === name.toLowerCase()){
-        sessionStorage.setItem("id", id);
-        sessionStorage.setItem("name", name);
-        window.location.href = "choose.html";
+        const hunterSpotsRef = db
+            .collection("reserved")
+            .doc("hunters")
+            .collection("hunterID")
+            .doc("h" + hID)
+            .collection("dates")
+            .doc(thisDate);
+
+        const hunterSpotsSnap = await hunterSpotsRef.get();
+        const cells = hunterSpotsSnap.data()?.cells;
+        thisHunterSpots = Array.isArray(cells) ? cells : [];
+
+        sessionStorage.setItem("id", ID);
+        sessionStorage.setItem("name", INITIAL);
+        if(thisHunterSpots.length > 0){
+            window.location.href = "choose.html";
+        } else{
+            window.location.href = 'quadrants.html';
+        }
     }
 }
 
